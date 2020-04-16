70af23c358f4888307184f58fd8256f8386396d1
Dasha <wqe>
2020-04-17 00:19:44 +0300

    mixed up names of files :)

--- test_conftest.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-
-# Указываем полный путь к geckodriver.exe на вашем ПК.
-driver = webdriver.Firefox('C:\\geckodriver\\geckodriver.exe')
-driver.get("http://www.google.com")
-
